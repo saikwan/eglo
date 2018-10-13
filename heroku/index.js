c922605dffff8244fdcf0918704d0c32e9cd9287
saikwan <43937150+saikwan@users.noreply.github.com>
2018-10-13 22:59:30 +0630

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -14,10 +14,10 @@ var xhub = require('express-x-hub');
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
 
-app.use(xhub({ algorithm: '8d73faadcbf8f96c15b8c9e36aa3bb46', secret: process.env.APP_SECRET }));
+app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
-var token = process.env.TOKEN || 'eglomyanmar';
+var token = process.env.TOKEN || 'token';
 var received_updates = [];
 
 app.get('/', function(req, res) {
