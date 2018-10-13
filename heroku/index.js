eb68ce531430423c2be1ff42b695c545cb622644
saikwan <43937150+saikwan@users.noreply.github.com>
2018-10-13 22:50:27 +0630

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -88,30 +88,4 @@ app.post('/webhook', (req, res) => {
   }
 
 });
-// Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
 
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "EAAN8s6MsDRcBADhwCSAeivHTBIpnfwDGwijbmacxPvsHZB4ZC5wdNs9nZCVxXYVcS77lXZCDZCYdx41ZCB72eWWrkE5X4ZChY4jRRD4xAaGIsPYh6lZBsT2vynnHvAeiHhQ0E81OjCgehh0PM7LCKs3fZBoQdQqBZCphmZB0BNBesRzzWEGZCyL3McSG"
-    
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-  
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
-    }
-  }
-});
